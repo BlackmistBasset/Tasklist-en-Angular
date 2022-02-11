5f4e72aec8309d1e2a1f625fd944250a8d91d85d
Basset <aldana.sorni@gmail.com>
2022-02-11 12:19:49 -0300

    componente lista de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "primera tarea asd",
+        day: "fecha ssd",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "segunda tarea asd",
+        day: "fecha ssd",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "tercera tarea asd",
+        day: "fecha ssd",
+        reminder: false
+    }
+]
