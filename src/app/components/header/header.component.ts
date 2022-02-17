555826b80e53dab07fdd67632fe9a0c6f4e62377
Basset <aldana.sorni@gmail.com>
2022-02-17 14:27:08 -0300

    correccion de un pequeño error

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'My Task-list';
-  showAddTask: boolean = true;
+  showAddTask: boolean = false;
   subscription?: Subscription;
 
   constructor(
